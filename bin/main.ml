183eba7cb826122631931900a47c60cf4be6c041
Jason Yu <yuzhijingcheng1996@hotmail.com>
2024-05-21 08:40:44 +0000

    Better error messages

--- bin/main.ml
+++ bin/main.ml
@@ -7,6 +7,12 @@ let () =
     let in_channel = open_in Sys.argv.(1) in
     let lexbuf = Lexing.from_channel in_channel in
     let cunit = Parser.cunit Lexer.read lexbuf in
-    codegen cunit;
-    close_in in_channel
+    close_in in_channel;
+    try
+      codegen cunit
+    with
+    | Codegen.BorrowCheckError msg -> Printf.eprintf "Borrow checking failed: %s\n" msg; exit 1
+    | Lang.TypeError msg -> Printf.eprintf "Type error: %s\n" msg; exit 1
+    | Codegen.CodegenError msg -> Printf.eprintf "Error during code generation: %s\n" msg; exit 1
+    ;
   end
