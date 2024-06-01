fe733dd10f204aff342dabb0d6fbb0436128d729
Jason Yu <yuzhijingcheng1996@hotmail.com>
2024-06-01 18:42:10 +0800

    Added more friendly syntax error messages

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,10 @@
 let () =
+  let handle_syntax_error lexbuf msg_opt =
+    let pos = Lexing.lexeme_start_p lexbuf
+    and msg = Option.value ~default:"Parsing error" msg_opt in
+    Printf.eprintf "Syntax error: %s (line %d, col %d)\n" msg pos.pos_lnum (pos.pos_cnum - pos.pos_bol);
+    exit 1
+  in
   let config = Anvil.Config.parse_args() in
   if config.input_filenames = [] then begin
     Printf.eprintf "Error: a file name must be supplied!\n";
@@ -7,7 +13,12 @@ let () =
     let open Anvil.Codegen in
     let in_channel = List.hd config.input_filenames |> open_in in
     let lexbuf = Lexing.from_channel in_channel in
-    let cunit = Parser.cunit Lexer.read lexbuf in
+    let cunit =
+      try Parser.cunit Lexer.read lexbuf
+      with
+      | Lexer.SyntaxError msg -> handle_syntax_error lexbuf (Some msg)
+      | _ -> handle_syntax_error lexbuf None
+    in
     close_in in_channel;
     try
       codegen cunit config
