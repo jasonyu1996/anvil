0fe886d4658ef740e042471b6912bebf339a90e2
Jason Yu <yuzhijingcheng1996@hotmail.com>
2025-02-03 07:41:29 +0000

    fix: added a two-round codegen mode to circumvent some comb loop issue

--- lib/config.ml
+++ lib/config.ml
@@ -1,12 +1,14 @@
 type compile_config = {
   verbose: bool;
   disable_lt_checks : bool;
+  two_round_graph: bool;
   input_filenames: string list;
 }
 
 let parse_args () : compile_config =
   let verbose = ref false
   and disable_lt_checks = ref false
+  and two_round_graph = ref false
   and input_filenames = ref [] in
   let add_input_filename s =
     input_filenames := s::!input_filenames
@@ -14,13 +16,15 @@ let parse_args () : compile_config =
   Arg.parse
     [
       ("-verbose", Arg.Set verbose, "Enable verbose output");
-      ("-disable-lt-checks", Arg.Set disable_lt_checks, "Disable lifetime/borrow-related checks")
+      ("-disable-lt-checks", Arg.Set disable_lt_checks, "Disable lifetime/borrow-related checks");
+      ("-two-round", Arg.Set two_round_graph, "Enable codegen of logic for two rounds")
     ]
     add_input_filename
-    "anvil [-verbose] [-disable-lt-checks] <file1> [<file2>] ...";
+    "anvil [-verbose] [-disable-lt-checks] [-two-round] <file1> [<file2>] ...";
   {
     verbose = !verbose;
     disable_lt_checks = !disable_lt_checks;
+    two_round_graph = !two_round_graph;
     input_filenames = !input_filenames;
   }
 
