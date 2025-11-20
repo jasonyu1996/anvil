1e2c5308e753ec3356b539c76ebf8ebf44631f27
adi4comp <adi4workspace@gmail.com>
2025-11-20 17:49:34 +0800

    change default to weaken data tc and fix some test cases

--- lib/config.ml
+++ lib/config.ml
@@ -10,7 +10,7 @@ type compile_config = {
 let parse_args () : compile_config =
   let verbose = ref false
   and disable_lt_checks = ref false
-  and weak_typecasts = ref false
+  and strong_typecasts = ref false
   and opt_level = ref 2
   and two_round_graph = ref false
   and input_filenames = ref [] in
@@ -21,7 +21,7 @@ let parse_args () : compile_config =
     [
       ("-verbose", Arg.Set verbose, "Enable verbose output");
       ("-disable-lt-checks", Arg.Set disable_lt_checks, "Disable lifetime/borrow-related checks");
-      ("-weak-typecasts", Arg.Set weak_typecasts, "Allow Typecasts");
+      ("-strict-typecasts", Arg.Set strong_typecasts, "Restrict Typecasts");
       ("-O", Arg.Set_int opt_level, "Set optimisation level: 0, 1, 2 (default)");
       ("-two-round", Arg.Set two_round_graph, "Enable codegen of logic for two rounds")
     ]
@@ -30,7 +30,7 @@ let parse_args () : compile_config =
   {
     verbose = !verbose;
     disable_lt_checks = !disable_lt_checks;
-    weak_typecasts = !weak_typecasts;
+    weak_typecasts = not !strong_typecasts;
     opt_level = !opt_level;
     two_round_graph = !two_round_graph;
     input_filenames = !input_filenames;
